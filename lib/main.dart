0a62e5c279d9d090f3c211583b360f2b2ba99b46
Arya_Lekshmi <aryaglekshmi@gmail.com>
2023-02-24 18:30:12 +0000

    Added username and password in Ui and displayed entered value

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
+import 'package:flutter/animation.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(myApp());
+  runApp(const myApp());
 }
 
 class myApp extends StatelessWidget {
@@ -10,28 +11,82 @@ class myApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(primaryColor: Colors.white60),
+      theme: ThemeData(primaryColor: const Color(0x99ad2c2c)),
       home: HomeScreen(),
     );
   }
 }
 
-class HomeScreen extends StatelessWidget {
-  const HomeScreen({Key? key}) : super(key: key);
+class HomeScreen extends StatefulWidget {
+  HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  final TextEditingController _username = TextEditingController();
+
+  final TextEditingController _password = TextEditingController();
+
+  String _usernamepassword = "";
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color(0xff9eab8e),
-      appBar: AppBar(
-        backgroundColor: Colors.white10,
-        title: const Text("Welcome to app"),
-      ),
-      body: const Center(
-        child: Text(
-          "This is your first page",
-          style: TextStyle(color: Colors.green),
+        backgroundColor: const Color(0xffdcaa12),
+        appBar: AppBar(
+          backgroundColor: Colors.black87,
+          title: const Text("Welcome to GCafe"),
         ),
-      ),
-    );
+        body: Padding(
+          padding: const EdgeInsets.all(20),
+          child: Container(
+            color: Colors.black26,
+            child: Column(children: [
+              Padding(
+                padding: const EdgeInsets.all(15),
+                child: TextField(
+                  controller: _username,
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'User Name',
+                    hintText: 'Enter Your Name',
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(15),
+                child: TextField(
+                  controller: _password,
+                  obscureText: true,
+                  enableSuggestions: false,
+                  autocorrect: false,
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Password',
+                    hintText: 'Enter Password',
+                  ),
+                ),
+              ),
+              ElevatedButton(
+                  onPressed: () {
+                    setState(() {
+                      _usernamepassword = "User name is " +
+                          _username.text +
+                          "and Password is " +
+                          _password.text;
+                      _password.text = "";
+                      _username.text = "";
+                    });
+                  },
+                  child: const Text("Print")),
+              Padding(
+                padding: const EdgeInsets.all(15),
+                child: Text(_usernamepassword),
+              )
+            ]),
+          ),
+        ));
   }
 }
